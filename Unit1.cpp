add3a23ab891f6491e3d7c4618bf61b26c565a35
krshnvshn <33021330+krshnvshn@users.noreply.github.com>
2017-11-06 14:25:22 +0300

    финальная версия

--- Unit1.cpp
+++ Unit1.cpp
@@ -4,8 +4,26 @@
  using namespace std;
 
   int main(int argc,char* argv[]){
-char str[100]="Hello world!",simbol;
+
+
+ char str[100]="Hello world!",simbol;
  cout << str << endl;
 
+    cout << "Enter simbol :";
+    cin >>simbol;
+    for(int i=0; str[i]!='\0';i++)
+        if (str[i]==simbol && (str[i-1]==' ' || i==0))
+    {
+        int j=1,k=i;
+        while (str[j]!=' ' && str[j]!='\0')j++;
+        for(k=i;str[k+j]!='\0';k++)
+            str[k]=str[k+j];
+          str[k]='\0';
+          i--;
+
+    cout<< "New string : "<<str<<endl;
+    }
+
+    getch();
     return 0;
     }
